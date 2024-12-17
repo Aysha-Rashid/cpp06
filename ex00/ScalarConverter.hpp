f7e72ef7f633d05e1c672b96d9526c0fc55e3d01
Aysha Rashid <130707249+Aysha-Rashid@users.noreply.github.com>
2024-12-17 20:32:26 +0400

    Add files via upload

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,17 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+class ScaleConvert
+{
+    private:
+
+    public:
+        static ScaleConvert convert(std::string string);
+        // toFloat
+        // toDouble
+        // toInt
+        // toChar ()
+};
+
+#endif
